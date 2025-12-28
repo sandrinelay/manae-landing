f5fb1cc3faa681110b4c835a8ee1032d2a42c88c
Sand <sandrine.layl@gmail.com>
2025-12-28 01:45:24 +0100

    fix: footer

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -12,7 +12,7 @@ export const Footer: React.FC = () => {
                 <span className="text-2xl font-bold text-neutral-900 font-logo">manae</span>
               </div>
               <p className="text-neutral-500 max-w-sm">
-                 Organise tes pensées. Agis sereinement.
+                 Dépose ce qui t'encombre. Manae trie, tu respires.
               </p>
            </div>
 
